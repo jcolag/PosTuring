dc53c4714ccd2adddd3acea24513bd5c0d1f0e85
John Colagioia <john@colagioia.net>
2014-07-08 07:59:59 -0400

    Kill spaces before parentheses

--- post-tur.c
+++ post-tur.c
@@ -3,15 +3,15 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-struct  tape_pos {
+struct tape_pos {
     char                symbol;
     struct tape_pos    *left,
                        *right;
 }                      *tape;
 
 /* The Memory of the machine, the instruction pointer, and the symbol table */
-int     CodeSpace[1024][2],
-        label[256];
+int CodeSpace[1024][2],
+    label[256];
 
 /***
  *** Function Prototypes
@@ -19,91 +19,91 @@ int     CodeSpace[1024][2],
 
 /* Utility functions */
 
-int parse_post_turing (FILE *, FILE *, int[][2], int *);
-void interpret_post_turing (int, int [][2]);
+int parse_post_turing(FILE *, FILE *, int[][2], int *);
+void interpret_post_turing(int, int [][2]);
 
 /* Language instructions */
 
-void Right (void);
-void Left (void);
-void Print (char);
-void IF (char, char, int *);
-void Display (int);
+void Right(void);
+void Left(void);
+void Print(char);
+void IF(char, char, int *);
+void Display(int);
 
 /***
  *** Main program
  ***/
 
-int main (int argc, char *argv[]) {
+int main(int argc, char *argv[]) {
     FILE  *infile;
     char   initial[128], *inputc;
     int    currinst = 0, idx;
 
-    tape = malloc (sizeof (struct tape_pos));
+    tape = malloc(sizeof(struct tape_pos));
     tape->right = tape->left = NULL;
     tape->symbol = '0';
 
-    for (idx = 0; idx < 256; idx++) {
+    for(idx = 0; idx < 256; idx++) {
         label[idx] = -1;
     }
 
-    if (argc < 2) {
-        printf (
+    if(argc < 2) {
+        printf(
           "\nUsage:  %s <filename>\n\tWhere <filename> a valid Post-Turing program.\n\n",
           argv[0]);
-        exit (0);
+        exit(0);
     }
 
-    infile = fopen (argv[1], "r");
-    if (infile == NULL) {
-        printf ("\nFile \"%s\" does not exist.\n\n", argv[1]);
-        exit (1);
+    infile = fopen(argv[1], "r");
+    if(infile == NULL) {
+        printf("\nFile \"%s\" does not exist.\n\n", argv[1]);
+        exit(1);
     }
 
     currinst = parse_post_turing(infile, stderr, CodeSpace, label);
 
-    printf ("Enter initial conditions:\n");
-    scanf ("%127[^\n\t]", initial);
-    for (inputc = initial, idx = 0; *inputc; inputc++, idx++) {
-        Print (*inputc);
-        Right ();
+    printf("Enter initial conditions:\n");
+    scanf("%127[^\n\t]", initial);
+    for(inputc = initial, idx = 0; *inputc; inputc++, idx++) {
+        Print(*inputc);
+        Right();
     }
 
-    for ( ; idx >= 0; idx--) {
-        Left ();
+    for( ; idx >= 0; idx--) {
+        Left();
     }
 
-    interpret_post_turing (currinst, CodeSpace);
+    interpret_post_turing(currinst, CodeSpace);
 
-    printf ("Program Terminated\n");
-    fclose (infile);
+    printf("Program Terminated\n");
+    fclose(infile);
     return 0;
 }
 /*
  * Core runtime engine.  Checks type of statement and executes
  * appropriate function.
  */
-void interpret_post_turing (int line, int code[1024][2]) {
+void interpret_post_turing(int line, int code[1024][2]) {
     int instruction;
 
-    Display (20);
-    for (instruction = 0; instruction < line; instruction++) {
-        switch (code[instruction][0]) {
+    Display(20);
+    for(instruction = 0; instruction < line; instruction++) {
+        switch(code[instruction][0]) {
         case 0:
-            Right ();
+            Right();
             break;
         case 1:
-            Left ();
+            Left();
             break;
         case 2:
-            Print ((char)code[instruction][1]);
+            Print((char)code[instruction][1]);
             break;
         default:                        /* It's a label reference */
-            IF ((char)(code[instruction][0] - 2), code[instruction][1], &instruction);
+            IF((char)(code[instruction][0] - 2), code[instruction][1], &instruction);
             break;
         }
 
-        Display (20);
+        Display(20);
     }
 }
 
@@ -113,20 +113,20 @@ void interpret_post_turing (int line, int code[1024][2]) {
  *   statements and ignores keywords that are only for structure.
  * Leaves a code identifying the statement type and parameter if any.
  */
-int parse_post_turing (FILE *file, FILE *errfile, int code[1024][2], int *labels) {
+int parse_post_turing(FILE *file, FILE *errfile, int code[1024][2], int *labels) {
     char    token[16];
     int     line = 0;
 
-    while (fscanf (file, "%15s", token) > 0) {
-        switch (tolower(token[0])) {
+    while(fscanf(file, "%15s", token) > 0) {
+        switch(tolower(token[0])) {
         case '[':                       /* Label */
             labels[token[1]] = line;        /* Label points to current line. */
             break;
         case 'i':                       /* If */
-            fscanf (file, "%15s", token);
+            fscanf(file, "%15s", token);
             code[line][0] = token[0] + 2;   /* Condition */
-            fscanf (file, "%15s", token);   /* Goto... */
-            fscanf (file, "%15s", token);
+            fscanf(file, "%15s", token);   /* Goto... */
+            fscanf(file, "%15s", token);
             code[line++][1] = token[0];     /* ...the label. */
             break;
         case 'l':                       /* Left */
@@ -134,15 +134,15 @@ int parse_post_turing (FILE *file, FILE *errfile, int code[1024][2], int *labels
             break;
         case 'p':                       /* Print */
             code[line][0] = 2;
-            fscanf (file, "%15s", token);
+            fscanf(file, "%15s", token);
             code[line++][1] = token[0];
             break;
         case 'r':                       /* Right */
             code[line++][0] = 0;
             break;
         default:                        /* Other */
-            fprintf (errfile, "Invalid program!!\n\n");
-            exit (2);
+            fprintf(errfile, "Invalid program!!\n\n");
+            exit(2);
             break;
         }
     }
@@ -153,29 +153,29 @@ int parse_post_turing (FILE *file, FILE *errfile, int code[1024][2], int *labels
 /*
  * Display() shows the local area of the tape around the head.
  */
-void Display (int nLeft) {
+void Display(int nLeft) {
     int         pos;
     static int  count = 0;
 
-    for (pos = 0; pos < nLeft; pos++) {
-        putchar (' ');
+    for(pos = 0; pos < nLeft; pos++) {
+        putchar(' ');
     }
 
-    putchar ('v');
-    putchar ('\n');
+    putchar('v');
+    putchar('\n');
 
-    for (pos = 0; pos < nLeft; pos++) {
-        Left ();
+    for(pos = 0; pos < nLeft; pos++) {
+        Left();
     }
-    for (pos = 0; pos < 79; pos++) {
-        putchar (tape->symbol);
-        Right ();
+    for(pos = 0; pos < 79; pos++) {
+        putchar(tape->symbol);
+        Right();
     }
-    for (pos = 0; pos < (79 - nLeft); pos++) {
-        Left ();
+    for(pos = 0; pos <(79 - nLeft); pos++) {
+        Left();
     }
 
-    putchar ('\n');
+    putchar('\n');
     ++count;
 }
 
@@ -186,9 +186,9 @@ void Display (int nLeft) {
 /*
  * Right() moves the tape head to the cell to the right.
  */
-void Right (void) {
-    if (tape->right == NULL) {
-        tape->right = malloc (sizeof (struct tape_pos));
+void Right(void) {
+    if(tape->right == NULL) {
+        tape->right = malloc(sizeof(struct tape_pos));
         tape->right->right = NULL;
         tape->right->left = tape;
         tape->right->symbol = '0';
@@ -200,9 +200,9 @@ void Right (void) {
 /*
  * Left()--surprise!--moves the tape head to the cell to the left.
  */
-void Left (void) {
-    if (tape->left == NULL) {
-        tape->left = malloc (sizeof (struct tape_pos));
+void Left(void) {
+    if(tape->left == NULL) {
+        tape->left = malloc(sizeof(struct tape_pos));
         tape->left->left = NULL;
         tape->left->right = tape;
         tape->left->symbol = '0';
@@ -214,7 +214,7 @@ void Left (void) {
 /*
  * Print() writes a symbol to the current tape cell.
  */
-void Print (char Symbol) {
+void Print(char Symbol) {
     tape->symbol = Symbol;
 }
 
@@ -222,11 +222,11 @@ void Print (char Symbol) {
  * IF() compares the current cell's contents to the symbol,
  * and transfers control to the label if there's a match.
  */
-void IF (char Symbol, char newlabel, int *instruction) {
-    if (tape->symbol == Symbol) {
-        if (label[(int)newlabel] == -1) {
-            printf ("\nProgram Terminated\n");
-            exit (0);
+void IF(char Symbol, char newlabel, int *instruction) {
+    if(tape->symbol == Symbol) {
+        if(label[(int)newlabel] == -1) {
+            printf("\nProgram Terminated\n");
+            exit(0);
         } else {
             *instruction = label[(int)newlabel] - 1;
         }
